f14c91a2c94bbacec8da047379b734c1d87ceb44
zaqlul9595 <cheezbawlz123@gmail.com>
2024-05-20 18:12:49 -0500

    Added the grenade and all its files

---
+++ ttt2-dbomb/gamemodes/terrortown/entities/weapons/weapon_ttt_dbomb/shared.lua
@@ -0,0 +1,67 @@
+SWEP.Author = "cheezbawlz"
+
+if SERVER then
+	AddCSLuaFile()
+end
+
+--Convars!
+CreateConVar("ttt_can_cook_dbomb", 1, {FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Allows the user to cook the grenade or not", 0)
+
+SWEP.Icon = "vgui/ttt/icon_dbomb"
+SWEP.PrintName = "Discombombulator"
+SWEP.Slot = 6
+
+SWEP.Base = "weapon_tttbasegrenade"
+
+SWEP.HoldType = "grenade"
+
+SWEP.Kind = WEAPON_EQUIP1
+
+SWEP.UseHands = true
+SWEP.ViewModelFlip = false
+SWEP.ViewModelFOV = 54
+SWEP.Spawnable = true
+SWEP.AutoSpawnable = false	
+SWEP.AllowDrop = true
+SWEP.Weight = 5
+
+SWEP.CanBuy = { ROLE_TRAITOR }
+
+SWEP.ViewModel = "models/weapons/cstrike/c_eq_fraggrenade.mdl"
+SWEP.WorldModel = "models/weapons/w_eq_fraggrenade.mdl"
+
+SWEP.EquipMenuData = {
+   type = "Discombombulator",
+   desc = "An explosive grenade that is identical to the discombobulator...`\nUntil the fuse goes off."
+};
+
+--Overides the base grenade Think function
+--since we want this nade to only start the fuse after we throw it
+--ISSUE: The fuse time displayed on screen is no longer accurate
+local cookCheck = GetConVar("ttt_can_cook_dbomb"):GetInt()
+if cookCheck == 0 then
+	function SWEP:Think()
+	   local ply = self:GetOwner()
+	   if not IsValid(ply) then return end
+	   -- returns true when the pin is pulled
+	   if self:GetPin() then
+		  -- we will throw now
+		  if not ply:KeyDown(IN_ATTACK) then
+			 self:StartThrow()
+			 self:SetPin(false)
+			 self:SendWeaponAnim(ACT_VM_THROW)
+			 --reset the detonation time to 5 seconds once thrown
+			 self:SetDetTime( CurTime() + 5 )	
+			 if SERVER then
+				self:GetOwner():SetAnimation( PLAYER_ATTACK1 )
+			 end
+		  end
+	   elseif self:GetThrowTime() > 0 and self:GetThrowTime() < CurTime() then
+		  self:SetDetTime( CurTime() + 5 )
+		  self:Throw()
+	   end
+	end
+end
+	function SWEP:GetGrenadeName()
+	   return "ttt_dbomb_proj"
+	end
---
+++ ttt2-dbomb/lua/autorun/server/force_download.lua
@@ -0,0 +1,4 @@
+--Missing icon fix
+if (SERVER) then
+	  resource.AddFile( "materials/vgui/ttt/icon_dbomb.vmt" )
+end
